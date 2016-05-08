e7ef5eb2e35800db6f6cb479e22f018528b9d15c
Maikel Withagen <maikel.withagen@gmail.com>
2016-05-08 21:17:25 +0200

    write the classified xml to output

--- code/recognizer.py
+++ code/recognizer.py
@@ -2,7 +2,7 @@ import sys, os, inspect, shutil
 import own_modules.preprocess as prep
 import toolbox.pamImage as pamImage
 import toolbox.croplib as croplib
-import xml.etree.ElementTree
+import xml.etree.ElementTree as ET
 # check commandline parameters
 if len(sys.argv) != 4:
     print "Usage: python %s image.ppm input.words /path/to/output.words" % sys.argv[0]
@@ -14,6 +14,7 @@ if not os.path.exists("tmp"):
 
 in_file_name = sys.argv[1]
 words_file_name = sys.argv[2]
+words_file_name_out = sys.argv[3]
 
 # Pre-proccesing steps
 
@@ -23,19 +24,36 @@ if prep.preprocess(in_file_name) != 0:
 else :
     print "Pre-processing completed"
 
+# Get the pre-processed image
 preIm = pamImage.PamImage("tmp/preprocessed.ppm")
-e = xml.etree.ElementTree.parse(words_file_name).getroot()
+e = ET.parse(words_file_name).getroot()
 
 print "Parsing pre-processed image into individual sentences"
 # Parse the given words (xml) file to find the word sections
-for child in e:
-    left = int(child.get('left'))
-    top = int(child.get('top'))
-    right = int(child.get('right'))
-    bottom = int(child.get('bottom'))
+for sentence in e:
+    left = int(sentence.get('left'))
+    top = int(sentence.get('top'))
+    right = int(sentence.get('right'))
+    bottom = int(sentence.get('bottom'))
     cropped_im = croplib.crop(preIm, left, top, right, bottom)
     cropped_im.thisown = True                 # to make Python cleanup the new C++ object afterwards
-    cropped_im.save('tmp/sentence'+child.get('no')+'.ppm')
+    cropped_im.save('tmp/sentence'+sentence.get('no')+'.ppm')
+
+    # Now to the individual words
+    for word in sentence:
+        left = int(word.get('left'))
+        top = int(word.get('top'))
+        right = int(word.get('right'))
+        bottom = int(word.get('bottom'))
+        cropped_im = croplib.crop(preIm, left, top, right, bottom)
+        cropped_im.thisown = True                 # to make Python cleanup the new C++ object afterwards
+        cropped_im.save('tmp/sentence'+sentence.get('no')+'_word'+word.get('no')+'.ppm')
+
+        # classify the word
+        word.set('text', 'dunno')
+        print "Set text: %s" % word.get('text')
 
 # Write results/delete the tmp folder
+tree = ET.ElementTree(e)
+tree.write(words_file_name_out)
 # shutil.rmtree('tmp')
