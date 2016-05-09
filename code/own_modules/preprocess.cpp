8a1edc6eb72f73a0bcc86aff491d7f6b0f21fb52
StevenBosch <stevenbosch@live.nl>
2016-05-09 18:32:37 +0200

    speck removal

--- code/own_modules/preprocess.cpp
+++ code/own_modules/preprocess.cpp
@@ -1,19 +1,40 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <stdio.h>
 
 using namespace cv;
 using namespace std;
 
 int preprocess (char* filename)
 {
+    Scalar color = Scalar(255, 255, 255);
+
     // Load the image
     Mat image = imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
     Mat result;
+
     // Convert to otsu
     threshold(image, result, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
 
-    // Other steps
-    //...
+    // Remove specks
+    Mat temp = result.clone();
+    vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
+    int mode = CV_RETR_LIST;
+    int method = CV_CHAIN_APPROX_NONE;
+
+    findContours(temp, contours, hierarchy, mode, method, Point(0, 0));
+
+    for (vector<vector<Point> >::iterator it = contours.begin(); it!=contours.end(); )
+    {
+      if (it->size() > 30)
+          it = contours.erase(it);
+      else it++;
+    }
+    for(unsigned i = 0; i < contours.size(); i++)
+      drawContours(temp, contours, i, color, 2, 8, hierarchy, 0, Point() );
+
+    result = result + temp;
 
     // Write to preprocecessed file
     string newName = "tmp/preprocessed.ppm";
---
+++ code/own_modules/run.py
@@ -0,0 +1,3 @@
+import preprocess as p
+a = p.preprocess('Stanford.ppm')
+#b = p.showPlaatje('tmp/preprocessed.ppm')
