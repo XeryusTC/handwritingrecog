72b0e4be2879eea5cbacd13b05b561848fa8e289
XeryusTC <armadillo@onenetbeyond.org>
2016-05-31 12:42:01 +0200

    Get rid of the toolbox in the recognizer

--- code/recognize/get_words.py
+++ code/recognize/get_words.py
@@ -1,38 +1,35 @@
 import sys, os, logging, shutil
 import xml.etree.ElementTree as ET
+from unipath import Path
+import cv2
+
+sides = ('left', 'top', 'right', 'bottom')
 
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
-import toolbox.croplib as croplib
-import toolbox.pamImage as pamImage
 
-def getWords(preIm, xml, sentenceDir, wordDir):
-    if os.path.exists(sentenceDir):
-        shutil.rmtree(sentenceDir)
-    os.makedirs(sentenceDir)
-    if os.path.exists(wordDir):
-        shutil.rmtree(wordDir)
-    os.makedirs(wordDir)
+def getWords(img, xml, sentenceDir, wordDir):
+    sentenceDir.rmtree()
+    sentenceDir.mkdir()
+    wordDir.rmtree()
+    wordDir.mkdir()
 
     # Parse the given words (xml) file to find the word sections
     logging.info("Cropping words")
     for sentence in xml:
-        left = int(sentence.get('left'))
-        top = int(sentence.get('top'))
-        right = int(sentence.get('right'))
-        bottom = int(sentence.get('bottom'))
-        cropped_im = croplib.crop(preIm, left, top, right, bottom)
-        cropped_im.thisown = True                 # to make Python cleanup the new C++ object afterwards
-        cropped_im.save(sentenceDir + 'sentence'+ sentence.get('no')+'.ppm')
+        rect = {side: int(sentence.get(side)) for side in sides}
+        no = sentence.get('no')
+        cropped_im = img[rect['top']:rect['bottom'],
+            rect['left']:rect['right']]
+        cv2.imwrite(sentenceDir.child('sentence' + no + '.ppm'), cropped_im)
 
         # Now to the individual words
         for word in sentence:
-            left = int(word.get('left'))
-            top = int(word.get('top'))
-            right = int(word.get('right'))
-            bottom = int(word.get('bottom'))
-            cropped_im = croplib.crop(preIm, left, top, right, bottom)
-            cropped_im.thisown = True                 # to make Python cleanup the new C++ object afterwards
-            cropped_im.save(wordDir + sentence.get('no') + '_word' + word.get('no')+'.ppm')
+            rect = {side: int(word.get(side)) for side in sides}
+            word_no = word.get('no')
+            cropped_im = img[rect['top']:rect['bottom'],
+                rect['left']:rect['right']]
+            cv2.imwrite(wordDir.child(no + '_word' + word_no + '.ppm'),
+                cropped_im)
 
             # classify the word
             # word.set('text', 'dunno')
